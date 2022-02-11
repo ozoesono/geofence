c2aada9cbd17fa6224be53891bfbbf59609c9048
ozoomosigho <ozoesono@gmail.com>
2022-02-11 23:22:36 +0000

    Initiatlise Geofence project

---
+++ src/main/java/com/rezolve/geofence/GeoFenceApplication.java
@@ -0,0 +1,13 @@
+package com.rezolve.geofence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoFenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoFenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rezolve/geofence/GeoFenceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.rezolve.geofence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoFenceApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
